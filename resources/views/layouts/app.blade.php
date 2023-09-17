da1e5d30d732ab826bf2c70f3953fc1e93795e22
Amatul Saboor Siddiqua <amatulsaboor221@gmail.com>
2023-09-17 14:36:36 +0500

    remove extra scripts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,20 +8,14 @@
     <meta http-equiv="pragma" content="no-cache">
     <meta http-equiv="expires" content="0">
     <meta name="csrf-token" id="csrf_token" content="{{ csrf_token() }}" />
-    
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.15.3/dist/sweetalert2.all.min.js"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="{{ asset('js/ajax_call.js') }}" defer></script>
     <script src="{{ asset('js/swal_popup.js') }}" defer></script>
-
-
     
     {{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> --}}
     {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css"> --}}
@@ -32,13 +26,9 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-
-
-  
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @stack('css')
 </head>
-
 <body>
     <div id="app">
 <!-- sidebar -->
@@ -73,7 +63,6 @@
             <!--  -->
             <div class="container ms-0">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    
                     <img src="{{url('/images/logo.PNG')}}" class="logo">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -81,16 +70,9 @@
                     aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                   
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                  
+                    <ul class="navbar-nav me-auto"></ul>
                     <ul class="navbar-nav ms-auto">
-                      
                         @guest
                         @if (Route::has('login'))
                         <li class="nav-item">
@@ -99,7 +81,6 @@
                             </button>
                         </li>
                         @endif
-
                         @if (Route::has('register'))
                         <li class="nav-item">
                             <button class="register-link ms-2">
@@ -120,7 +101,6 @@
                                                      document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
                                 </a>
-
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                     @csrf
                                 </form>
@@ -131,16 +111,10 @@
                 </div>
             </div>
         </nav>
-
         <main class="py-4">
             @yield('content')
         </main>
     </div>
-    @stack('js')
-    
-
-
-
 </body>
-
+@stack('js')
 </html>
